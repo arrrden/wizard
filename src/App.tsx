42c5edc9e034d0598df57b88f5d4d879b047fb8c
Arden <j.gaddas@outlook.com>
2021-05-12 21:00:02 +0100

    create blank CRA with deps

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {Button} from 'antd'
+
+const App = () => {
+  return (
+    <div>
+      <Button onClick={() => window.alert("boop")}>Next</Button>
+    </div>
+  );
+}
+
+export default App;
